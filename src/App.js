3d5dd137df519ea544cece4dfa4e8c7647c591a4
Jatou Gaye <jatougaye@gmail.com>
2022-08-28 21:02:43 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import Header from "./components/Header";
+import React, { useState, useEffect } from "react";
+import MovieScreen from "./components/MovieScreen";
+import Watchlist from "./components/Watchlist";
 
 function App() {
+  // Create three pieces of state
+  const [movieList, setMovieList] = useState([]);
+  const [list, setList] = useState([]);
+  const [page, setPage] = useState(1);
+
+  const getData = () => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=${page}`
+      )
+      .then((res) => {
+        console.log(res.data.results);
+        setMovieList(res.data.results);
+      });
+  };
+
+  useEffect(() => {
+    getData();
+  }, [page]);
+
+  // add movie
+  const addMovie = (movie) => setList([...list, movie]);
+
+  // remove movie
+  const removeMovie = (movie) => {
+    const newState = list.filter((mov) => {
+      return mov !== movie;
+    });
+    setList(newState);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Header />
+         <MovieScreen
+          movieList={movieList}
+          page={page}
+          setPage={setPage}
+          list={list}
+          addMovie={addMovie}
+          removeMovie={removeMovie}
+        />
+        <Watchlist list={list} removeMovie={removeMovie}/>
     </div>
   );
 }
